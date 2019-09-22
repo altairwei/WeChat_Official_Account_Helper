ab9480f17f29cb20a42d3b60844128229015f384
Altair Wei <altair_wei@outlook.com>
2019-09-22 23:59:51 +0800

    add query_image

--- pub_article.sh
+++ pub_article.sh
@@ -5,20 +5,26 @@
 #       - jq
 
 # Parse args
-index_file=$1
-index_file_basename=$(basename "${index_file}")
-index_dirname=$(dirname "${index_file}")
-index_temp_file="${index_dirname}/${index_file_basename}.tmp"
-cat "${index_file}" > "${index_temp_file}"
+parse_args() {
+    index_file=$1
+    index_file_basename=$(basename "${index_file}")
+    index_dirname=$(dirname "${index_file}")
+    index_temp_file="${index_dirname}/${index_file_basename}.tmp"
+    cat "${index_file}" > "${index_temp_file}"
+}
+
 
 # Get AppID and AppSecret
 AppID=$(head -n 1 appid.txt)
 AppSecret=$(tail -n 1 appid.txt)
 
 # Get Acess Token
-ACESS_TOKEN_API_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${AppID}&secret=${AppSecret}"
-res_json=$(http --print b ${ACESS_TOKEN_API_URL})
-access_token=$(printf ${res_json} | jq -r '.access_token')
+get_acess_token() {
+    ACESS_TOKEN_API_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${AppID}&secret=${AppSecret}"
+    res_json=$(http --print b ${ACESS_TOKEN_API_URL})
+    access_token=$(printf ${res_json} | jq -r '.access_token')
+    echo ${access_token}
+}
 
 # Get all materials
 #API_URL="https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=${access_token}"
@@ -32,24 +38,32 @@ upload_image() {
 }
 
 query_image() {
+    local image_name=$1
+    local access_token=$(get_acess_token)
     # Get image count
     local api_url="https://api.weixin.qq.com/cgi-bin/material/get_materialcount?access_token=${access_token}"
     local res_json=$(http --print b ${api_url})
     local image_count=$(printf ${res_json} | jq -r '.image_count')
     # Get image list
     local api_url="https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=${access_token}"
-    while (( image_count > 0 )); do
-        local res_json=$(http -p b -j ${api_url} type=image offset=0 count=20)
-        image_count=$((image_count - 20))
+    local offset=0
+    while (( offset < image_count )); do
+        local res_json=$(http -p b -j ${api_url} type=image offset=${offset} count=20)
+        printf ${res_json} | jq '.item'
+        # Move offset to next point
+        offset=$((offset + 20))
     done
-    }
+}
+
+query_image
 
 # Parse image urls from markdwn file
-
-while read url; do
-    printf 'Uploading image: %s\n' "${url}"
-    #TODO: upload images
-    
-    sed -E -e "s/${url}//"
-done < <(cat "${index_file}" | sed -nE -e 's/.*!\[.*\]\((.*)\).*/\1/p')
+parse_iamge() {
+    while read url; do
+        printf 'Uploading image: %s\n' "${url}"
+        #TODO: upload images
+        
+        sed -E -e "s/${url}//"
+    done < <(cat "${index_file}" | sed -nE -e 's/.*!\[.*\]\((.*)\).*/\1/p')
+}
 
